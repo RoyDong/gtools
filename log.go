3af9bb5732779c28e331ad27c56cc7e8c4488e61
Roy Dong <i@roydong.com>
2017-04-06 00:48:35 +0800

    .

--- log.go
+++ log.go
@@ -11,23 +11,6 @@ var (
 )
 
 
-func initLogger(name string) *log.Logger {
-    conf := Store.Tree("config.log")
-
-    file, has := conf.String(name)
-    if !has {
-        file = "log" + string(os.PathSeparator) + name + ".log"
-    }
-
-    out, err := createLogfile(file)
-    if err == nil {
-        logger := log.New(out, "gmvc", 1)
-        logger.SetFlags(log.LstdFlags)
-        return logger
-    }
-    return nil
-}
-
 func createLogfile(filename string) (*os.File, error) {
     var f *os.File
     var e error
